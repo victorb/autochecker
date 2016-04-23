f048fa7a2e9aa2bb78cf4e24280685181f54f4b5
Victor Bjelkholm <victorbjelkholm@gmail.com>
2016-04-23 11:04:46 +0200

    Fix java tests

--- examples/java/CalculatorTest.java
+++ examples/java/CalculatorTest.java
@@ -7,6 +7,6 @@ public class CalculatorTest {
   public void evaluatesExpression() {
     Calculator calculator = new Calculator();
     int sum = calculator.evaluate("1+2+3");
-    assertEquals(9, sum);
+    assertEquals(6, sum);
   }
 }
