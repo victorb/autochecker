83af7a1cae860326ef2e8736227fba3360e61d13
Victor Bjelkholm <victorbjelkholm@gmail.com>
2016-04-23 11:37:43 +0200

    Add clojure example

---
+++ examples/clojure/autochecker.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+# Bu, only one alpine version available :/
+../../cli.js lein-2.6.1-alpine
---
+++ examples/clojure/test/autotest_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns autotest-example.core-test
+  (:require [clojure.test :refer :all]
+            [autotest-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
